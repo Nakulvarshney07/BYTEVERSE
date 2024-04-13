a4db84b0ca392447c041508cd51ae06d7d22da64
anshulvyas67 <anshulv.ug23.cs@nitp.ac.in>
2024-04-13 14:06:01 +0530

    login and sign in page added

--- lib/initialpages/Login_Page.dart
+++ lib/initialpages/Login_Page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:huma/initialpages/Signup_page.dart';
 
 class LoginPage extends StatefulWidget{
   @override
@@ -17,6 +18,7 @@ class _LoginPageState extends State<LoginPage> {
     final _width=MediaQuery.of(context).size.width;
     final _height=MediaQuery.of(context).size.height;
     return Scaffold(
+
       body: Stack(
         children: [
           Container(
@@ -25,7 +27,7 @@ class _LoginPageState extends State<LoginPage> {
 
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/anshulassets/finalbackground.jpg'),
+                image: AssetImage("assets/anshulassets/finalbackground.jpg"),
                 fit: BoxFit.cover,
               ),
 
@@ -50,10 +52,11 @@ class _LoginPageState extends State<LoginPage> {
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
                 child: Container(
-                  width: 200, // Width of child container
-                  height: 300, // Height of child container
+                  width: 250, // Width of child container
+                  height: 370, // Height of child container
 
                   decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
                       border: Border.all(
                         width: 1,
                         color: Colors.transparent.withOpacity(0.1),
@@ -70,21 +73,147 @@ class _LoginPageState extends State<LoginPage> {
                       children: [
                         SizedBox(height: 10,),
                         Text('User Login' ,
-                          style: TextStyle(fontStyle: FontStyle.italic,
-                          color: Colors.white.withOpacity(0.7),
+                          style: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
                           fontSize: 25,
                           fontWeight: FontWeight.bold,
                           ),
                         ),
+                        SizedBox(height: 15,),
                         Form(
                             child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                TextFormField(
-                                  controller: emailcontroller,
+                                Form(
+                                    key: formkey,
+                                    child: Column(
+                                      children: [
+                                        SizedBox(
+                                          child: TextFormField(
+                                            controller: emailcontroller,
+                                            decoration: InputDecoration(
+                                                hintText: 'Enter your Email',
+                                                enabledBorder: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(18),
+                                                    borderSide: BorderSide(
+                                                        color: Colors.grey,
+                                                        width: 2
+                                                    )
+                                                ),
+                                                focusedBorder: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(18),
+                                                    borderSide: BorderSide(
+                                                        color: Colors.grey,
+                                                        width: 2
+                                                    )
+                                                ),
+                                                errorBorder: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(18),
+                                                    borderSide: BorderSide(
+                                                        color: Colors.red,
+                                                        width: 2
+                                                    )
+                                                ),
+                                                focusedErrorBorder:  OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(18),
+                                                  borderSide: BorderSide(
+                                                      color: Colors.grey,
+                                                      width: 2
+                                                  ),),
 
-                                )
+                                                prefixIcon: Icon(Icons.alternate_email,color: Colors.grey,)
+                                            ),
+                                            validator:  (value){
+                                              if(value!.isEmpty){
+                                                return 'Enter Correct Email';
+
+                                              }
+                                              return null;
+                                            },
+                                          ),
+                                          width: 200,
+                                          height: 80,
+
+                                        ),
+                                        SizedBox(
+                                          width: 200,
+                                          height: 80,
+                                          child: TextFormField(
+                                            controller: passcontroller,
+                                            decoration: InputDecoration(
+                                                hintText: 'Enter your Passowrd',
+                                                enabledBorder: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(18),
+                                                    borderSide: BorderSide(
+                                                        color: Colors.grey,
+                                                        width: 2
+                                                    )
+                                                ),
+                                                focusedBorder: OutlineInputBorder(
+                                                    borderRadius: BorderRadius.circular(18),
+                                                    borderSide: BorderSide(
+                                                        color: Colors.grey,
+                                                        width: 2
+                                                    )
+                                                ),
+                                                errorBorder: OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(18),
+                                                  borderSide: BorderSide(
+                                                      color: Colors.red,
+                                                      width: 2
+                                                  ),
+                                                ),
+                                                focusedErrorBorder:  OutlineInputBorder(
+                                                  borderRadius: BorderRadius.circular(18),
+                                                  borderSide: BorderSide(
+                                                      color: Colors.grey,
+                                                      width: 2
+                                                  ),),
+                                                prefixIcon: Icon(Icons.password_sharp,color: Colors.grey,)
+                                            ),
+                                            validator:  (value){
+                                              if(value!.isEmpty){
+                                                return 'Enter Correct Password';
+                                                setState(() {
+
+                                                });
+
+                                              }
+                                              return null;
+                                            },
+                                          ),
+                                        ),
+                                        SizedBox(height: 3.5,),
+                                        ElevatedButton(
+                                          style: ButtonStyle(
+                                            backgroundColor: MaterialStateProperty.all(Colors.grey),
+                                          ),
+                                            onPressed: (){
+                                              if (formkey.currentState!.validate())  {
+
+                                              }
+                                            },
+                                            child: Text('Login' , style: TextStyle(color: Colors.black,fontSize: 21,fontWeight: FontWeight.w400),)),
+                                        SizedBox(height:15 ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Text('Create new Account?'),
+                                            SizedBox(width: 20,),
+                                            InkWell(
+                                              onTap: (){
+                                           Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignUp()));
+                                              },
+                                              child: Text('Sign up', style: TextStyle(
+                                                  color: Colors.green.withOpacity(0.8)
+                                              ),),
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ))
                               ],
-                            )
+                            ),
                         )
                       ],
                     ),
